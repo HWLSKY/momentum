df2652d7578a464ca396eae7521785a58ee661a8
HWLSKY <32188641+HWLSKY@users.noreply.github.com>
2024-03-03 11:57:23 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -4,6 +4,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `./img/${chosenImage}`;
+bgImage.src = `https://hwlsky.github.io/momentum/img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
